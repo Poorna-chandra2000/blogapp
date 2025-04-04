e94507e4bea870a392a28aa5556ab3cdf2d167a5
Poorna Chandra2000 <poornachandra473@gmail.com>
2025-04-05 00:50:31 +0530

    Google cloud storage demo

--- src/main/java/com/rahul/rahulapp/Services/BlogService.java
+++ src/main/java/com/rahul/rahulapp/Services/BlogService.java
@@ -2,6 +2,7 @@ package com.rahul.rahulapp.Services;
 
 import com.rahul.rahulapp.Dtos.BlogDto;
 import com.rahul.rahulapp.Entities.Blog;
+import com.rahul.rahulapp.GoogleCloudStorageService.GCSService;
 import com.rahul.rahulapp.Repositories.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -23,24 +24,49 @@ public class BlogService {
     private final BlogRepository blogRepository;
     private final ModelMapper modelMapper;
     private final FileService fileService;
+    private final GCSService gcsService;
 
     @Value("${project.image}")
     private String imagePath;
 
-    public BlogDto saveBlog(BlogDto blogDto, MultipartFile image) throws IOException {
-// Upload the image
+    //upgrade 1 blog can have multiple images use list<String> images
+    //add each url to list
 
-        String imgpathurl = fileService.uploadFile(imagePath, image);
+    //next deletion of just images separately
+
+
+//    public BlogDto saveBlog(BlogDto blogDto, MultipartFile image) throws IOException {
+//   // Upload the image and store it in server
+//
+//        String imgpathurl = fileService.uploadFile(imagePath, image);
+//
+//        // Create the URL for the uploaded image
+//       String imageUrl = "/images/" + imgpathurl;
+//
+//        Blog blog = modelMapper.map(blogDto, Blog.class);
+//        blog.setImgurl(imageUrl);
+//
+//
+//        return modelMapper.map(blogRepository.save(blog), BlogDto.class);
+//    }
+
+    public BlogDto saveBlog(BlogDto blogDto, MultipartFile image) throws IOException {
+        // Upload the image and store it in server
+        gcsService.uploadFile(image);
+        //for cloud storage
+        String imageUrl = gcsService.uploadFile(image);;
 
         // Create the URL for the uploaded image
-//        String imageUrl = "http://localhost:8080/images/" + fileName;
+        //String imageUrl = "/images/" + imgpathurl;
 
         Blog blog = modelMapper.map(blogDto, Blog.class);
-        blog.setImgurl(imgpathurl);
+        blog.setImgurl(imageUrl);
 
 
         return modelMapper.map(blogRepository.save(blog), BlogDto.class);
     }
+
+
 
     public String saveimg(Long id, MultipartFile image) throws IOException {
         Blog blog = blogRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Blog not found with id: {}" + id));
